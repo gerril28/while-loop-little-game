aa47c06a10946e990ee4be9b796d7fcdcbcdb281
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-05 09:53:38 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     Player.change(LedSpriteProperty.X, -1)
 })
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showNumber(Points)
+    game.resume()
+})
 input.onButtonPressed(Button.B, function () {
     Player.change(LedSpriteProperty.X, 1)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    game.pause()
+    basic.pause(100)
+    game.resume()
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 100; index++) {
@@ -14,17 +23,16 @@ input.onGesture(Gesture.Shake, function () {
             }
             if (Player.isTouching(Enemy)) {
                 Enemy.delete()
-                Points += 1
+                Points = Points + 1
+                basic.showNumber(Points)
+                basic.clearScreen()
             }
-            basic.showNumber(Points)
-            basic.clearScreen()
         }
-        for (let index = 0; index < 1; index++) {
-            Enemy = game.createSprite(randint(0, 4), 1)
-            if (Player.isTouching(Player)) {
-                Enemy.delete()
-                Points += 1
-            }
+        Player = game.createSprite(2, 0)
+        Enemy = game.createSprite(randint(0, 4), 1)
+        if (Player.isTouching(Player)) {
+            Enemy.delete()
+            Points = Points + 1
             basic.showNumber(Points)
             basic.clearScreen()
         }
