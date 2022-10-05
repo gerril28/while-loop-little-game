572a8b5592b5ba1daabb80f7c837eb644bff034b
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-05 09:38:39 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,16 @@ input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 100; index++) {
         for (let index = 0; index < 1; index++) {
             for (let index = 0; index < 4; index++) {
-                Enemy.change(LedSpriteProperty.X, 1)
+                basic.pause(100)
+                Player.change(LedSpriteProperty.Y, 1)
                 basic.pause(100)
             }
-            if (Player.isTouching(Player)) {
+            if (Player.isTouching(Enemy)) {
                 Enemy.delete()
                 Points += 1
             }
+            basic.showNumber(Points)
+            basic.clearScreen()
         }
         for (let index = 0; index < 1; index++) {
             Enemy = game.createSprite(randint(0, 4), 1)
@@ -22,14 +25,17 @@ input.onGesture(Gesture.Shake, function () {
                 Enemy.delete()
                 Points += 1
             }
+            basic.showNumber(Points)
+            basic.clearScreen()
         }
     }
 })
+let Points = 0
 let Enemy: game.LedSprite = null
 let Player: game.LedSprite = null
 Player = game.createSprite(2, 0)
-Enemy = game.createSprite(randint(0, 4), 2)
-let Points = 0
+Enemy = game.createSprite(randint(0, 4), 4)
+Points = 0
 basic.forever(function () {
 	
 })
